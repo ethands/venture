b3be7d309cf7944a1526415fbde6c9243d21c049
qiyichen <416350144@qq.com>
2019-05-15 09:49:45 +0800

    Initial commit

---
+++ src/main/java/com/jiaozi/venture/demo/VentureApplication.java
@@ -0,0 +1,13 @@
+package com.jiaozi.venture.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VentureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VentureApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jiaozi/venture/demo/VentureApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jiaozi.venture.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VentureApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
